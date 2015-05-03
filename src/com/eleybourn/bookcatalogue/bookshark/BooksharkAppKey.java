39bf7de69de2e7c768732cb0c6237799b4c08a90
Dimitris Klisiaris <dklisiaris@gmail.com>
2015-05-03 19:34:48 +0300

    Enable greek language, fix some translations.

---
+++ src/com/eleybourn/bookcatalogue/bookshark/BooksharkAppKey.java
@@ -0,0 +1,16 @@
+package com.eleybourn.bookcatalogue.bookshark;
+
+/**
+ * Bookshark is a book metadata service for books published in greece, powered by biblionet.gr and bibliography.gr
+ *
+ * NOTE: The project must include a class, BooksharkAppKey in this folder, which contains a
+ * single static String, KEY, containing the app key granted by Bookshark. For testing purposes
+ * this KEY can be junk.
+ *
+ * If you need to test the app, and do not have an Bookshark App Key, just use this file.
+ *
+ * @author dklisiaris
+ */
+public class BooksharkAppKey  {
+    public static final String KEY = "foo";
+}
---
+++ src/com/eleybourn/bookcatalogue/bookshark/BooksharkUtils.java
@@ -0,0 +1,17 @@
+package com.eleybourn.bookcatalogue.bookshark;
+
+/**
+ * Bookshark is a book metadata service for books published in greece, powered by biblionet.gr and bibliography.gr
+ *
+ * Wrappers for Bookshark API
+ *
+ * NOTE: The project must include a class, BooksharkAppKey in this folder, which contains a
+ * single static String, KEY, containing the app key granted by Bookshark. For testing purposes
+ * this KEY can be junk.
+ *
+ * @author dklisiaris
+ *
+ */
+
+public class BooksharkUtils {
+}
